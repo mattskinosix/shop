43796bb77644801e3781f003c849566ed10ea3e8
Matteo Greco <matteogreco9@gmail.com>
2020-05-11 12:25:38 +0000

    aggiunta pulsanti e fine barcode con autorizzazione
    Signed-off-by: Matteo Greco <matteogreco9@gmail.com>

--- app/src/main/java/c/www/shop/menu/menu.java
+++ app/src/main/java/c/www/shop/menu/menu.java
@@ -1,6 +1,7 @@
 package c.www.shop.menu;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -9,12 +10,16 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import c.www.shop.R;
 import c.www.shop.categorie.MainActivity;
+import c.www.shop.products.products;
 import c.www.shop.qrcode.ScannedBarcodeActivity;
+import c.www.shop.registrazione.registrazione;
 
 public class menu extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 101;
@@ -22,11 +27,39 @@ public class menu extends AppCompatActivity {
     private static final String SAVED_INSTANCE_URI = "uri";
     private static final String SAVED_INSTANCE_RESULT = "result";
     private static final int REQUEST_CAMERA_PERMISSION = 200;
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.general_menu);
+        Button account_setting=findViewById(R.id.account_setting);
+        account_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(menu.this, registrazione.class);
+                //start the second Activity
+                startActivity(intent);
+            }
+        });
+        Button special_products=findViewById(R.id.promotions);
+        special_products.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(menu.this, products.class);
+                //start the second Activity
+                startActivity(intent);
+            }
+        });
+        Button promotions=findViewById(R.id.special_product);
+        promotions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(menu.this, products.class);
+                //start the second Activity
+                startActivity(intent);
+            }
+    });
         Button productInStock= findViewById(R.id.product_in_stock);
         productInStock.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,7 +79,41 @@ public class menu extends AppCompatActivity {
             }
         });
     }
+    public boolean checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
 
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                new AlertDialog.Builder(this)
+                        .setTitle(R.string.title_location_permission)
+                        .setMessage(R.string.text_location_permission)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(menu.this,
+                                        new String[]{Manifest.permission.CAMERA},
+                                        MY_PERMISSIONS_REQUEST_CAMERA);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_CAMERA);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
